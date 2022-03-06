bce671d34f5be926faf07f3cf1323387ff709645
MTHLily <tree.of.pyres@gmail.com>
2022-03-07 00:44:00 +0800

    Added doom-emacs

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elfeed-feeds '("http://news.mth-lily.com/i/?a=rss"))
+ '(org-modules '(ol-bibtex org-habit org-notify org-toc)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
